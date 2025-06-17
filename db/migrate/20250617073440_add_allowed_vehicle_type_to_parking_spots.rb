dd09268b3b0d70e1de9a891e3b5072890310f128
Francesco Mari <mari@adobe.com>
2025-06-17 09:51:16 +0200

    Properly generate the DB migration

--- db/migrate/20250617073440_add_allowed_vehicle_type_to_parking_spots.rb
+++ db/migrate/20250617073440_add_allowed_vehicle_type_to_parking_spots.rb
@@ -1,4 +1,4 @@
-class AddAllowedVehicleTypeToParkingSpots < ActiveRecord::Migration[7.0]
+class AddAllowedVehicleTypeToParkingSpots < ActiveRecord::Migration[7.1]
   def change
     add_column :parking_spots, :allowed_vehicle_type, :integer, null: false, default: 0
   end
